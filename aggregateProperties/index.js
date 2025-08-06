6254a4ae38163c4030574597d5d77c37ca50d491
im-Ahmed <ahmedfarooqim3@gmail.com>
2025-08-06 18:03:34 +0500

    first commit

---
+++ aggregateProperties/index.js
@@ -0,0 +1,30 @@
+const data = [
+  { userId: "u1", event: "login", timestamp: 1678886400 },
+  { userId: "u2", event: "logout", duration: 120 },
+  { userId: "u1", event: "pageview", pageId: "home" },
+  { userId: "u3", event: "login", timestamp: 1678886500, region: "us" },
+];
+
+const aggregateProperties = (data) => {
+    if (!data || data.length === 0) {
+      return {};
+    }
+  
+    return data.reduce((accumulator, currentObject) => {
+      for (const key in currentObject) {
+        if (Object.prototype.hasOwnProperty.call(currentObject, key)) {
+          if (!accumulator[key]) {
+            // If the key doesn't exist, initialize it with an array containing the value
+            accumulator[key] = [currentObject[key]];
+          } else {
+            // If the key exists, push the new value to its array
+            accumulator[key].push(currentObject[key]);
+          }
+        }
+      }
+      return accumulator;
+    }, {}); // The initial value for the accumulator is an empty object
+  };
+  
+  const result = aggregateProperties(data);
+  console.table(result);
